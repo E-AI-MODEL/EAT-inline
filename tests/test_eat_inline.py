35e9f69d71f6acb9b16965218a078df6987ce1d9
EAI <vis@emmauscollege.nl>
2026-07-22 21:07:23 +0200

    Align tests with single-construct core

--- tests/test_eat_inline.py
+++ tests/test_eat_inline.py
@@ -1,6 +1,6 @@
 import unittest
 
-from eat_inline import parse, parse_references, parse_tldr_blocks, validate_reference
+from eat_inline import parse, parse_references, validate_reference
 
 
 class EatInlineTests(unittest.TestCase):
@@ -10,32 +10,28 @@ class EatInlineTests(unittest.TestCase):
         self.assertEqual(len(refs), 2)
         self.assertEqual(refs[0].type, "person")
         self.assertEqual(refs[0].key, "Hans_Visser")
-        self.assertEqual(refs[0].status, "valid-core")
+        self.assertEqual(refs[1].type, "organisation")
 
-    def test_unknown_type_is_extension(self):
+    def test_unknown_type_is_still_syntactically_valid(self):
         refs = parse_references("@@EAT research_method:Controlled_Experiment@@")
-        self.assertEqual(refs[0].status, "valid-extension")
+        self.assertEqual(len(refs), 1)
+        self.assertEqual(refs[0].type, "research_method")
 
     def test_invalid_identifier(self):
         valid, code = validate_reference("@@EAT person:Hans Visser@@")
         self.assertFalse(valid)
-        self.assertEqual(code, "E005 invalid_identifier")
+        self.assertEqual(code, "invalid_identifier")
 
     def test_missing_closing_marker(self):
         valid, code = validate_reference("@@EAT person:Hans_Visser")
         self.assertFalse(valid)
-        self.assertEqual(code, "E006 missing_closing_marker")
-
-    def test_tldr_block(self):
-        text = "@@EAT tldr:\nEAT Inline maakt referenties expliciet.\n@@"
-        blocks = parse_tldr_blocks(text)
-        self.assertEqual(len(blocks), 1)
-        self.assertEqual(blocks[0].content, "EAT Inline maakt referenties expliciet.")
+        self.assertEqual(code, "missing_closing_marker")
 
     def test_parse_shape(self):
         result = parse("@@EAT person:Hans_Visser@@")
-        self.assertEqual(result["version"], "0.3.1")
+        self.assertEqual(result["version"], "0.3.2")
         self.assertEqual(len(result["references"]), 1)
+        self.assertNotIn("tldr", result)
 
 
 if __name__ == "__main__":
